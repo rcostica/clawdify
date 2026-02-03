1d20a4b98804c21c2a1dbfd1f82088c89204ac39
Razvan Costica <rcostica@gmail.com>
2026-02-03 13:44:10 +0200

    Fix resizable component types for react-resizable-panels compatibility
    - Updated imports to use PanelGroup, Panel, PanelResizeHandle
    - Updated type imports to PanelGroupProps, PanelProps, PanelResizeHandleProps
    - Removed unused pg dependency (was only for migration attempt)
    - Build passes cleanly

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,16 +1,23 @@
 "use client"
 
 import { GripVerticalIcon } from "lucide-react"
-import * as ResizablePrimitive from "react-resizable-panels"
+import {
+  Panel,
+  PanelGroup,
+  PanelResizeHandle,
+  type PanelGroupProps,
+  type PanelProps,
+  type PanelResizeHandleProps,
+} from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 function ResizablePanelGroup({
   className,
   ...props
-}: ResizablePrimitive.GroupProps) {
+}: PanelGroupProps) {
   return (
-    <ResizablePrimitive.Group
+    <PanelGroup
       data-slot="resizable-panel-group"
       className={cn(
         "flex h-full w-full aria-[orientation=vertical]:flex-col",
@@ -21,19 +28,19 @@ function ResizablePanelGroup({
   )
 }
 
-function ResizablePanel({ ...props }: ResizablePrimitive.PanelProps) {
-  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
+function ResizablePanel({ ...props }: PanelProps) {
+  return <Panel data-slot="resizable-panel" {...props} />
 }
 
 function ResizableHandle({
   withHandle,
   className,
   ...props
-}: ResizablePrimitive.SeparatorProps & {
+}: PanelResizeHandleProps & {
   withHandle?: boolean
 }) {
   return (
-    <ResizablePrimitive.Separator
+    <PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
@@ -46,7 +53,7 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </ResizablePrimitive.Separator>
+    </PanelResizeHandle>
   )
 }
 
